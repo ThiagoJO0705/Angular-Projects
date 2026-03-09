4a0042e426b67e71106bd909a25bcdf11d1d4e05
ThiagoJO0705 <rm551624@fiap.com.br>
2026-03-09 14:24:46 -0300

    importando o projeto de calculadora

---
+++ calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss'
+})
+export class CalculatorComponent {
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+
+  calcularResultado(){
+    console.log("Chamando o método para calcular o resultado!");
+    console.log("Numero1: " + this.number1);
+    console.log("Numero2: " + this.number2);
+    this.result = this.number1 + this.number2;
+  }
+}
